7facef1281e02a6d09c96377c8868f1d8fa4396c
jmartins9 <joaoantonioredondo1994@gmail.com>
2022-10-23 17:19:27 +0100

    Logs adicionado

---
+++ DNSServer/src/Logs.java
@@ -0,0 +1,51 @@
+/********************************************************
+ * Author: João Martins                                 *
+ * Created date: 22/10/2022                             *
+ * Last Update: 22/10/2022                              *
+ ********************************************************/
+import java.net.InetAddress;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+
+public class Logs {
+    public enum EntryType {
+        QR, QE, RP, RR, ZT, EV, ER, EZ, FL, TO, SP, ST
+    }
+
+    final static DateFormat dateFormat = new SimpleDateFormat("dd:MM:yyyy.HH:mm:ss:mmm");  // .format
+
+    Date date; /* Etiqueta temporal */
+    EntryType type; /* Tipo de entrada */
+    InetAddress addr; /* Endereço IP */
+    int port; /* Porta */
+    Byte[] data; /* Dados de entrada */
+
+    public Logs (Date date, EntryType type, InetAddress addr, int port, Byte[] data) {
+        this.date = date;
+        this.type = type;
+        this.addr = addr;
+        this.port = port;
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(dateFormat.format(this.date));
+        sb.append(" ");
+        sb.append(type);
+        sb.append(addr);
+        if (port!=-1) {       /* Ainda vou mudar isto */
+            sb.append(":");
+            sb.append(port);
+        }
+        sb.append(" ");
+        sb.append(Arrays.toString(data));
+        sb.append("\n");
+        return sb.toString();
+    }
+
+}
