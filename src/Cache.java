183589c36fae329a9b7f12e1a5efd5253a78704b
JoseBambora <zebragapt@gmail.com>
2022-11-08 21:44:46 +0000

    Updated cache module in order to save the SP database on cache.

--- src/Cache.java
+++ src/Cache.java
@@ -11,18 +11,12 @@ import java.util.*;
  */
 public class Cache
 {
-    private final Map<Tuple<String,Byte>,EntryCache> cacheRV;
-    private final Map<Tuple<String,Byte>,EntryCache> cacheAV;
-    private final Map<Tuple<String,Byte>,EntryCache> cacheEV;
-    private final Map<Tuple<String,Byte>,LocalDateTime> times;
+    private final Map<Tuple<String,Byte>,EntryCache> cache;
     private int espaco;
     private int answers;
     public Cache(int espaco)
     {
-        this.cacheRV = new HashMap<>();
-        this.cacheAV = new HashMap<>();
-        this.cacheEV = new HashMap<>();
-        this.times = new HashMap<>();
+        this.cache = new HashMap<>();
         this.espaco = espaco;
         this.answers = 0;
     }
@@ -33,53 +27,39 @@ public class Cache
 
     public void addData(DNSPacket resposta, EntryCache.Origin origin)
     {
+        this.removeExpireInfo();
         Data data = resposta.getData();
         Tuple<String,Byte> t = new Tuple<>(data.getName(), data.getTypeOfValue());
         if(this.answers == this.espaco)
             this.removeData();
-        if(!this.cacheRV.containsKey(t))
+        if(!this.cache.containsKey(t))
         {
+            EntryCache entryCache = new EntryCache(resposta,origin);
             this.answers++;
-            this.cacheRV.put(t,new EntryCache(data.getResponseValues(),origin));
-            this.cacheAV.put(t,new EntryCache(data.getResponseValues(),origin));
-            this.cacheEV.put(t,new EntryCache(data.getResponseValues(),origin));
-            this.times.put(t,LocalDateTime.now());
+            this.cache.put(entryCache.getKey(),entryCache);
         }
         else if(origin == EntryCache.Origin.OTHERS)
-            this.times.put(t,LocalDateTime.now());
+            this.cache.get(t).setTempoEntrada(LocalDateTime.now());
+    }
+    public void addData(String dom, byte type, Value valor, EntryCache.Origin origin)
+    {
+        Tuple<String,Byte> t = new Tuple<>(dom, type);
+        if(!this.cache.containsKey(t))
+            this.cache.put(t,new EntryCache(t, origin));
+        this.cache.get(t).addValueDB(valor);
     }
     public void removeExpireInfo()
     {
-        for(Tuple<String,Byte> key : this.cacheRV.keySet())
-        {
-            EntryCache rv = this.cacheRV.get(key);
-            EntryCache av = this.cacheAV.get(key);
-            EntryCache ev = this.cacheEV.get(key);
-            LocalDateTime localDateTime = this.times.get(key);
-            int num = rv.removeExpired(localDateTime);
-            av.removeExpired(localDateTime);
-            ev.removeExpired(localDateTime);
-            if(num == 0)
-            {
-                this.cacheRV.remove(key);
-                this.cacheAV.remove(key);
-                this.cacheEV.remove(key);
-            }
-        }
+        this.cache.values().forEach(EntryCache::removeExpireInfo);
     }
     public void removeData()
     {
-        this.removeExpireInfo();
         if(this.answers == this.espaco)
         {
-            List<Tuple<String,Byte>> remove = new ArrayList<>(this.cacheRV.keySet());
-            remove.sort((t1,t2) -> (int) ChronoUnit.NANOS.between(this.times.get(t2),this.times.get(t1)));
+            List<EntryCache> remove = new ArrayList<>(this.cache.values());
+            remove.sort((t1,t2) -> (int) ChronoUnit.NANOS.between(t2.getTempoEntrada(),t1.getTempoEntrada()));
             for(int i = 0; i < this.espaco/2; i++)
-            {
-                this.cacheRV.remove(remove.get(i));
-                this.cacheAV.remove(remove.get(i));
-                this.cacheEV.remove(remove.get(i));
-            }
+                this.cache.remove(remove.get(i).getKey());
             this.answers -= (this.espaco/2);
         }
     }
@@ -89,35 +69,33 @@ public class Cache
         byte b = mensagem.getData().getTypeOfValue();
         Tuple<String,Byte> t = new Tuple<>(name,b);
         Data res = null;
-        if(this.cacheRV.containsKey(t))
-        {
-            res =  new Data(name,b);
-            res.setResponseValues(this.cacheRV.get(t).getDados());
-            res.setAuthoriteValues(this.cacheAV.get(t).getDados());
-            res.setExtraValues(this.cacheEV.get(t).getDados());
-        }
+        if(this.cache.containsKey(t))
+            res = this.cache.get(t).getData();
+        return res;
+    }
+
+    public Data findAnswer(String dom, byte type)
+    {
+        Tuple<String,Byte> t = new Tuple<>(dom,type);
+        Data res = null;
+        if(this.cache.containsKey(t))
+            res = this.cache.get(t).getData();
         return res;
     }
 
     // exclusiva dos SS
     public void removeByName(String name)
     {
-        for(Tuple<String,Byte> elem : this.cacheRV.keySet())
-        {
+        for(Tuple<String,Byte> elem : this.cache.keySet())
             if(elem.getValue1().equals(name))
-            {
-                this.cacheRV.remove(elem);
-                this.cacheAV.remove(elem);
-                this.cacheEV.remove(elem);
-            }
-        }
+                this.cache.remove(elem);
     }
 
     @Override
     public String toString()
     {
         StringBuilder res = new StringBuilder();
-        for(EntryCache entryCache : this.cacheRV.values())
+        for(EntryCache entryCache : this.cache.values())
         {
             res.append(entryCache.toString()).append("\n");
         }
--- src/EntryCache.java
+++ src/EntryCache.java
@@ -13,45 +13,73 @@ public class EntryCache
 {
 
     public enum Origin {FILE, SP, OTHERS }
-    private List<Value> dados;
+    private Tuple<String,Byte> key;
+    private List<Value> responseValues;
+    private List<Value> authorityValues;
+    private List<Value> extraValues;
     private Origin origem;
+    private LocalDateTime tempoEntrada;
 
-    public EntryCache(Value[] dados, Origin origem)
+    public EntryCache(Tuple<String,Byte> key,Origin origem)
     {
-        this.dados = new ArrayList<>(List.of(dados));
+        this.key = key;
+        this.responseValues = new ArrayList<>();
+        this.authorityValues = new ArrayList<>();
+        this.extraValues = new ArrayList<>();
         this.origem = origem;
+        this.tempoEntrada = LocalDateTime.now();
     }
 
-    public Value[] getDados() {
-        return dados.toArray(new Value[1]);
-    }
-
-    public Origin getOrigem() {
-        return origem;
-    }
-
-    public int removeExpired(LocalDateTime localDateTimeAdded)
+    public EntryCache(DNSPacket dnsPacket, Origin origem)
     {
-        List<Integer> index = new ArrayList<>();
-        int i = 0;
-        for(Value value : dados)
+        this.key = new Tuple<>(dnsPacket.getData().getName(),dnsPacket.getData().getTypeOfValue());
+        this.responseValues = new ArrayList<>(List.of(dnsPacket.getData().getResponseValues()));
+        this.authorityValues = new ArrayList<>(List.of(dnsPacket.getData().getAuthoriteValues()));
+        this.extraValues = new ArrayList<>(List.of(dnsPacket.getData().getExtraValues()));
+        this.origem = origem;
+        this.tempoEntrada = LocalDateTime.now();
+    }
+
+    public LocalDateTime getTempoEntrada() {
+        return tempoEntrada;
+    }
+
+    public void removeExpireInfo()
+    {
+        if(this.origem != Origin.FILE)
         {
-            if(value.getTTL() < ChronoUnit.SECONDS.between(localDateTimeAdded,LocalDateTime.now()))
-            {
-                index.add(i);
-            }
-            i++;
+            this.responseValues  = this.responseValues.stream().filter(v -> v.getTTL() < ChronoUnit.SECONDS.between(LocalDateTime.now(), tempoEntrada)).toList();
+            this.authorityValues = this.authorityValues.stream().filter(v -> v.getTTL() < ChronoUnit.SECONDS.between(LocalDateTime.now(), tempoEntrada)).toList();
+            this.extraValues     = this.extraValues.stream().filter(v -> v.getTTL() < ChronoUnit.SECONDS.between(LocalDateTime.now(), tempoEntrada)).toList();
         }
-        index.sort((n1,n2) -> n2 - n1);
-        for(int in : index)
-        {
-            this.dados.remove(in);
-        }
-        return this.dados.size();
+    }
+
+    public Tuple<String, Byte> getKey() {
+        return key;
+    }
+
+    public void setTempoEntrada(LocalDateTime tempoEntrada) {
+        this.tempoEntrada = tempoEntrada;
+    }
+
+    public Data getData()
+    {
+        Value[] res = null;
+        if(!responseValues.isEmpty()) res = responseValues.toArray(new Value[1]);
+        Value[] aut = null;
+        if(!authorityValues.isEmpty()) aut = authorityValues.toArray(new Value[1]);
+        Value[] ext = null;
+        if(!extraValues.isEmpty()) ext = extraValues.toArray(new Value[1]);
+        return new Data(key.getValue1(), key.getValue2(),res,aut,ext);
+    }
+
+    public void addValueDB(Value value)
+    {
+        this.responseValues.add(value);
     }
 
     @Override
     public String toString() {
-        return this.dados.toString();
+        return this.getData().toString();
     }
 }
